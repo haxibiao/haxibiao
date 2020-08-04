e99a0ff93090f86c8e449039ba647b8b1c5a4994
ivan <ivan@haxibiao.com>
2020-08-04 11:54:11 +0800

    修复安卓打包apk几个小问题：code-push, multiDexEnabled,subprojects

---
+++ bash/apk.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+yel=$'\e[1;33m'
+grn=$'\e[1;32m'
+red=$'\e[1;31m'
+end=$'\e[0m'
+
+app="haxibiao"
+
+# cd $PWD
+
+if [ "$1" = "upload" ]; then
+
+echo "${yel}开始上传 apk 正式包"
+node ./bash/nodejs/cos_upload_apk.js release
+
+echo  -e "${grn}上传完成${end}"
+# echo "下载地址：http://$app-1251052432.cos.ap-shanghai.myqcloud.com/$app-release.apk"
+echo "下载地址：http://cos.${app}.com/${app}-release.apk"
+
+else
+
+echo "${grn}开始生成正式包 ..."
+cd ./android
+./gradlew assembleRelease 
+
+fi
